ca241262f73e9880b5ee152a6b1fddb19bf3231c
AlexandrSeleznyov <vuavso16@gmail.om>
2021-12-02 18:05:00 +0200

    fix
    erfe

--- model/user.js
+++ model/user.js
@@ -18,13 +18,17 @@ const userSchema = Schema(
       required: true,
       minlength: 6,
     },
-    token: {
-      type: String,
-      default: null,
-    },
   },
   { versionKey: false, timestamps: true }
 );
+
+// userSchema.methods.setPassword = function (password) {
+//   this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+// };
+
+// userSchema.methods.comparePassword = function (password) {
+//   return bcrypt.compareSync(password, this.password);
+// };
 
 const joiRegisterSchema = Joi.object({
   name: Joi.string().required(),
